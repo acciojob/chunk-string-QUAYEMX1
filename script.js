e86d0bbd535e975e43994bf3999fe0a3d5bab42b
QUAYEMX1 <126396459+QUAYEMX1@users.noreply.github.com>
2024-09-15 17:26:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function stringChop(str, size) {
-  // your code here
+  if (!str) {
+    return [];
+  }
+ 
+  const chunks = [];
+  for (let i = 0; i < str.length; i += size) {
+    chunks.push(str.substr(i, size));
+  }
+ 
+  return chunks;
 }
-
+ 
 // Do not change the code below
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
 alert(stringChop(str, size));
+ 
